b5facf69f0054b919e1d5c708bf1e4387ee01084
Mariela Stoycheva <megaira@abv.bg>
2020-04-11 18:08:23 +0300

    initial

---
+++ src/graphql/types/User.js
@@ -0,0 +1,24 @@
+export default `
+
+    type User {
+        _id: String!
+        username: String!
+        firstname: String!
+        lastname: String!
+        email: String!
+        password: String!
+        phone: String
+    }
+
+    type Query {
+        user(_id: String!): User
+        users: [User]
+    }
+
+    type Mutation {
+        addUser(username: String!, firstname: String!, lastname: String!, email: String!, password: String!, phone: String): User
+        deleteUser(_id: String!): User
+        editUser(_id: String!, username: String, firstname: String, lastname: String, email: String, password: String, phone: String): User
+    }
+
+`
---
+++ src/models/User.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: false
+    }
+})
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
